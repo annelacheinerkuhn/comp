a71bd726bf291503968072dd212ec3b6bf0ef0dd
annelacheinerkuhn <annelacheinerkuhn@gmail.com>
2016-03-07 19:48:54 +0000

    draft2

--- twitter_comp copy.rb
+++ twitter_comp copy.rb
@@ -1,23 +1,4 @@
 
-require 'sinatra'
-
-get('/ro') do
-  erb :comp
-end
-
-get('/:name') do
-  @name = params[:name]
-  erb :comp
-end
-
-post {'/signup'} do
-	puts params[:name]
-	puts params[:email]
-	"Cheers for providing ya name and email cutface"
-end
-
-
-
 require 'twitter'
 
 twitter_api = Twitter::REST::Client.new do |config|
